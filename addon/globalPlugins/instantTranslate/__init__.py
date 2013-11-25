33f1472ea33070bd497b6e00cf199828acd99f35
Alberto Buffolino <a.buffolino@gmail.com>
2013-11-25 12:17:49 +0100

    Fixed: boolean values returned from config must be compared to string values with first capitalized letter

--- addon/globalPlugins/instantTranslate/__init__.py
+++ addon/globalPlugins/instantTranslate/__init__.py
@@ -26,6 +26,7 @@ from time import sleep
 import threading
 import _config
 import addonHandler
+
 _config.load()
 _addonDir = os.path.join(os.path.dirname(__file__), "..", "..").decode("mbcs")
 _curAddon = addonHandler.Addon(_addonDir)
@@ -56,13 +57,13 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		lang_swap = _config.instanttranslateConfig['translation']['swap']
 		copyTranslation = autoSwap = isAutoSwapped = False
 		# determine whether to copy translation on clipboard
-		if _config.instanttranslateConfig['settings']['copytranslatedtext'] == 'true':
+		if _config.instanttranslateConfig['settings']['copytranslatedtext'] == 'True':
 			copyTranslation = True
 		# determine whether to swap automatically lang_swap and target language, if source recognized equal to the target
-		if _config.instanttranslateConfig['settings']['autoswap'] == 'true':
+		if _config.instanttranslateConfig['settings']['autoswap'] == 'True':
 			autoSwap = True
 		# keep track if there was a swapping from source=auto during previous NVDA session
-		if _config.instanttranslateConfig['temporary']['isautoswapped'] == 'true':
+		if _config.instanttranslateConfig['temporary']['isautoswapped'] == 'True':
 			isAutoSwapped = True
 
 	def createMenu(self):
