1ba864438c625523ed782d936bb891af4a0833de
Beqa Gozalishvili <beqaprogger@gmail.com>
2022-05-18 04:52:03 +0400

    NVDA 2022.1 compatibility

--- buildVars.py
+++ buildVars.py
@@ -27,7 +27,7 @@ addon_info = {
 	# Minimum NVDA version supported
 	"addon_minimumNVDAVersion" : "2018.3",
 	# Last NVDA version supported/tested
-	"addon_lastTestedNVDAVersion" : "2021.1"
+	"addon_lastTestedNVDAVersion" : "2022.1"
 }
 
 import os.path
