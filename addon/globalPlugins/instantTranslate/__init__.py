8ba5654dfa31a3092157b52473fa59f932694ed6
Cyrille Bougot <cyrille.bougot2@laposte.net>
2024-03-22 08:00:07 +0100

    Add the support for on-demand mode (#31)
    * Add the support for on-demand mode
    
    * Convert to script decorator
    
    * Update uncommitted file: funcExecutor.py should be compatible with all versions of NVDA.
    
    * * Rework so that all speech on demand support is in a dedicated module.
    * Restore 2023.3 behaviour when swapping languages
    
    * Remove credit following Paul's request
    
    * Fix compatibility with NVDA 2019.3
    
    * Fix linting (spaces) and a typo

--- addon/globalPlugins/instantTranslate/__init__.py
+++ addon/globalPlugins/instantTranslate/__init__.py
@@ -10,6 +10,7 @@
 from functools import wraps, lru_cache
 from .interface import InstantTranslateSettingsPanel
 from .langslist import g
+from .speechOnDemand import getSpeechOnDemandParameter, executeWithSpeakOnDemand
 from locale import getdefaultlocale
 from time import sleep
 from tones import beep
@@ -65,19 +66,20 @@ confspec = {
 "replaceUnderscores": "boolean(default=false)",
 }
 
+# Define speakOnDemand parameter for all scripts needing it
+speakOnDemand = getSpeechOnDemandParameter()
+
 # Below toggle code came from Tyler Spivey's code, with enhancements by Joseph Lee.
 
 def finally_(func, final):
 	"""Calls final after func, even if it fails."""
-	def wrap(f):
-		@wraps(f)
-		def new(*args, **kwargs):
-			try:
-				func(*args, **kwargs)
-			finally:
-				final()
-		return new
-	return wrap(final)
+	@wraps(func)
+	def new(*args, **kwargs):
+		try:
+			func(*args, **kwargs)
+		finally:
+			final()
+	return new
 
 #def detect_language(text):
 #	response=urllib.urlopen("https://translate.yandex.net/api/v1.5/tr.json/detect?key=trnsl.1.1.20150410T053856Z.1c57628dc3007498.d36b0117d8315e9cab26f8e0302f6055af8132d7&"+urllib.urlencode({"text":text.encode('utf-8')})).read()
@@ -131,6 +133,9 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	def script_error(self, gesture):
 		tones.beep(120, 100)
 
+	@scriptHandler.script(
+		description=_("Instant Translate layer commands. t translates selected text, shift+t translates clipboard text, a announces current swap configuration, s swaps source and target languages, c copies last result to clipboard, i identify the language of selected text, l translates last spoken text, o opens translation setting dialog.")
+	)
 	def script_ITLayer(self, gesture):
 		# A run-time binding will occur from which we can perform various layered translation commands.
 		# First, check if a second press of the script was done.
@@ -140,11 +145,15 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		self.bindGestures(self.__ITGestures)
 		self.toggling = True
 		tones.beep(100, 10)
-	script_ITLayer.__doc__=_("Instant Translate layer commands. t translates selected text, shift+t translates clipboard text, a announces current swap configuration, s swaps source and target languages, c copies last result to clipboard, i identify the language of selected text, l translates last spoken text, o opens translation setting dialog.")
 
 	def terminate(self):
 		gui.settingsDialogs.NVDASettingsDialog.categoryClasses.remove(InstantTranslateSettingsPanel)
 
+	@scriptHandler.script(
+		# Translators: message presented in input help mode, when user presses the shortcut keys for this addon.
+		description=_("Translates clipboard text from one language to another using Google Translate."),
+		**speakOnDemand,
+	)
 	def script_translateClipboardText(self, gesture):
 		try:
 			text = api.getClipData()
@@ -155,8 +164,6 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			ui.message(_("There is no text on the clipboard"))
 		else:
 			threading.Thread(target=self.translate, args=(text,self.lang_from, self.lang_to,)).start()
-	# Translators: message presented in input help mode, when user presses the shortcut keys for this addon.
-	script_translateClipboardText.__doc__=_("Translates clipboard text from one language to another using Google Translate.")
 
 	def getSelectedText(self):
 		obj=api.getCaretObject()
@@ -167,6 +174,11 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		except (RuntimeError, NotImplementedError):
 			return None
 
+	@scriptHandler.script(
+		# Translators: message presented in input help mode, when user presses the shortcut keys for this addon.
+		description=_("Translates selected text from one language to another using Google Translate."),
+		**speakOnDemand,
+	)
 	def script_translateSelection(self, gesture):
 		text = self.getSelectedText()
 		if not text:
@@ -174,8 +186,6 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			ui.message(_("no selection"))
 			return
 		threading.Thread(target=self.translate, args=(text,self.lang_from,self.lang_to,)).start()
-	# Translators: message presented in input help mode, when user presses the shortcut keys for this addon.
-	script_translateSelection.__doc__=_("Translates selected text from one language to another using Google Translate.")
 
 	def translate(self, text, langFrom, langTo):
 		if self.replaceUnderscores:
@@ -190,7 +200,7 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			return
 		self.lastTranslation = result.translation
 		msgTranslation = {'text': result.translation, 'lang': result.lang_to}
-		queueHandler.queueFunction(queueHandler.eventQueue, messageWithLangDetection, msgTranslation)
+		queueHandler.queueFunction(queueHandler.eventQueue, lambda: executeWithSpeakOnDemand(messageWithLangDetection, msgTranslation))
 		self.copyResult(result.translation)
 
 	@lru_cache()
@@ -233,6 +243,10 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	def swapLanguages(self, langFrom, langTo):
 		self.lang_from, self.lang_to = langTo, langFrom
 
+	@scriptHandler.script(
+		# Translators: Presented in input help mode.
+		description=_("It swaps source and target languages."),
+	)
 	def script_swapLanguages(self, gesture):
 		if self.lang_from == "auto":
 			self.swapLanguages(self.lang_swap, self.lang_to)
@@ -246,16 +260,26 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		ui.message(_("Languages swapped"))
 		# Translators: message presented to announce the current source and target languages.
 		ui.message(_("Translate: from {lang1} to {lang2}").format(lang1=self.lang_from, lang2=self.lang_to))
-		self.script_translateSelection(gesture)
-	# Translators: Presented in input help mode.
-	script_swapLanguages.__doc__ = _("It swaps source and target languages.")
+		try:
+			# NVDA 2024.1+
+			shouldTranslate = speech.getState().speechMode != speech.SpeechMode.onDemand
+		except AttributeError:
+			# NVDA <= 2023.3
+			shouldTranslate = True
+		if shouldTranslate:
+			self.script_translateSelection(gesture)
 
+	@scriptHandler.script(
+		# Translators: Presented in input help mode.
+		description=_("It announces the current source and target languages."),
+		**speakOnDemand,
+	)
 	def script_announceLanguages(self, gesture):
 		# Translators: message presented to announce the current source and target languages.
 		ui.message(_("Translate: from {lang1} to {lang2}").format(lang1=self.lang_from, lang2=self.lang_to))
-	# Translators: Presented in input help mode.
-	script_announceLanguages.__doc__ = _("It announces the current source and target languages.")
 
+	@scriptHandler.script(
+	)
 	def script_copyLastResult(self, gesture):
 		if self.lastTranslation:
 			self.copyResult(self.lastTranslation, ignoreSetting=True)
@@ -265,8 +289,13 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			# Translators: message presented to announce no previous translation disponibility
 			ui.message(_("No stored translation"))
 	# Translators: Presented in input help mode.
-	script_copyLastResult.__doc__ = _("It copies the last translation to clipboard")
+	description=_("It copies the last translation to clipboard"),
 
+	@scriptHandler.script(
+		# Translators: Presented in input help mode.
+		description=_("It identifies the language of selected text"),
+		**speakOnDemand,
+	)
 	def script_identifyLanguage(self, gesture):
 		text = self.getSelectedText()
 		if not text:
@@ -286,27 +315,33 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		myTranslator.join()
 		language = myTranslator.lang_detected
 		queueHandler.queueFunction(queueHandler.eventQueue, ui.message, g(language))
-	# Translators: Presented in input help mode.
-	script_identifyLanguage.__doc__ = _("It identifies the language of selected text")
 
 	def _localSpeak(self, sequence, *args, **kwargs):
 		self._speak(sequence, *args, **kwargs)
 		self.lastSpokenText = speechViewer.SPEECH_ITEM_SEPARATOR.join([x for x in sequence if isinstance(x, str)])
 
+	@scriptHandler.script(
+		# Translators: Presented in input help mode.
+		description=_("It translates the last spoken text"),
+		**speakOnDemand,
+	)
 	def script_translateLastSpokenText(self, gesture):
 		self.lastSpokenText and threading.Thread(target=self.translate, args=(self.lastSpokenText, self.lang_from, self.lang_to)).start()
-	# Translators: Presented in input help mode.
-	script_translateLastSpokenText.__doc__ = _("It translates the last spoken text")
 
+	@scriptHandler.script(
+		# Translators: Presented in input help mode.
+		description=_("Announces all available layered commands"),
+		**speakOnDemand,
+	)
 	def script_displayHelp(self, gesture):
 		ui.message(_("t translates selected text, shift+t translates clipboard text, a announces current swap configuration, s swaps source and target languages, c copies last result to clipboard, i identify the language of selected text, l translates last spoken text, o open translation settings dialog, h displays this message."))
-	# Translators: Presented in input help mode.
-	script_displayHelp.__doc__ = _("Announces all available layered commands")
 
+	@scriptHandler.script(
+		# Translators: Presented in input help mode.
+		description=_("Opens Instant Translate settings dialog."),
+	)
 	def script_showSettings(self, gesture):
 		wx.CallAfter(gui.mainFrame._popupSettingsDialog, gui.settingsDialogs.NVDASettingsDialog, InstantTranslateSettingsPanel)
-	# Translators: Presented in input help mode.
-	script_showSettings.__doc__ = _("Opens Instant Translate settings dialog.")
 
 	__ITGestures={
 		"kb:t":"translateSelection",
